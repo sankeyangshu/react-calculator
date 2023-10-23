64cf10d0539128bea2d634599c1c952204591963
sankeyangshu <1260323835@qq.com>
2023-10-23 11:00:21 +0800

    feat: 计算器页面样式完成

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -5,7 +5,34 @@ const Home = () => {
   return (
     <div className="home">
       <div className="result"></div>
-      <div className="content">123</div>
+      <div className="content">
+        <button className="item">C</button>
+        <button className="item">
+          <span className="division">&divide;</span>
+        </button>
+        <button className="item">
+          <span className="division">&times;</span>
+        </button>
+        <button className="item-blue">Del</button>
+        <button className="item">7</button>
+        <button className="item">8</button>
+        <button className="item">9</button>
+        <button className="item-blue">
+          <SwitchDark />
+        </button>
+        <button className="item">4</button>
+        <button className="item">5</button>
+        <button className="item">6</button>
+        <button className="item-blue">-</button>
+        <button className="item">1</button>
+        <button className="item">2</button>
+        <button className="item">3</button>
+        <button className="item-blue">+</button>
+        <button className="item">%</button>
+        <button className="item">0</button>
+        <button className="item">.</button>
+        <button className="item-blue">=</button>
+      </div>
     </div>
   );
 };
