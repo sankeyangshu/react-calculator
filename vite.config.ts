352c74989314d8fb46f813c8e424c238082a6a25
sankeyangshu <1260323835@qq.com>
2023-10-25 13:39:05 +0800

    build: 新增工作流

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // 将根路径换成相对路径
   plugins: [
     react(),
     //  使用 svg 图标
